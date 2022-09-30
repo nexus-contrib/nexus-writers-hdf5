4ab50ca403b7d1c9207b37b597d460ac15c48288
Apollo3zehn <git@m1.apollo3zehn.net>
2022-09-30 14:42:19 +0200

    Prepare release.

---
+++ src/Nexus.Writers.Hdf5/Hdf5.cs
@@ -0,0 +1,324 @@
+﻿using HDF.PInvoke;
+using Microsoft.Extensions.Logging;
+using Nexus.DataModel;
+using Nexus.Extensibility;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace Nexus.Writers
+{
+    [DataWriterDescription(DESCRIPTION)]
+    [ExtensionDescription(
+        "Store data in the HDF5 1.10 file format.",
+        "https://github.com/Apollo3zehn/nexus-writers-hdf5",
+        "https://github.com/Apollo3zehn/nexus-writers-hdf5")]
+    public class Hdf5 : IDataWriter
+    {
+        #region "Fields"
+
+        private const string DESCRIPTION = @"
+{
+  ""label"": ""HDF5 1.10 (*.h5)""
+}
+        ";
+
+        private long _fileId = -1;
+        private TimeSpan _lastSamplePeriod;
+        private static readonly JsonSerializerOptions _serializerOptions;
+
+        #endregion
+
+        #region Constructors
+
+        static Hdf5()
+        {
+            _serializerOptions = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+        }
+
+        #endregion
+
+        #region Properties
+
+        private DataWriterContext Context { get; set; } = default!;
+
+        #endregion
+
+        #region "Methods"
+
+        public Task SetContextAsync(
+           DataWriterContext context,
+           ILogger logger,
+           CancellationToken cancellationToken)
+        {
+            Context = context;
+            return Task.CompletedTask;
+        }
+
+        public Task OpenAsync(
+            DateTime fileBegin,
+            TimeSpan filePeriod,
+            TimeSpan samplePeriod,
+            CatalogItem[] catalogItems,
+            CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                _lastSamplePeriod = samplePeriod;
+
+                var totalLength = (ulong)(filePeriod.Ticks / samplePeriod.Ticks);
+                var root = Context.ResourceLocator.ToPath();
+                var filePath = Path.Combine(root, $"{fileBegin.ToString("yyyy-MM-ddTHH-mm-ss")}Z_{samplePeriod.ToUnitString()}.h5");
+
+                if (File.Exists(filePath))
+                    throw new Exception($"The file {filePath} already exists. Extending an already existing file with additional resources is not supported.");
+
+                try
+                {
+                    _fileId = H5F.create(filePath, H5F.ACC_TRUNC);
+
+                    if (_fileId < 0)
+                        throw new Exception($"{ErrorMessage.Hdf5Writer_CouldNotOpenOrCreateFile} File: {filePath}.");
+
+                    // file
+                    PrepareStringAttribute(_fileId, "date_time", fileBegin.ToString("yyyy-MM-ddTHH-mm-ssZ"));
+                    PrepareStringAttribute(_fileId, "sample_period", samplePeriod.ToUnitString());
+
+                    foreach (var catalogItemGroup in catalogItems.GroupBy(catalogItem => catalogItem.Catalog))
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+
+                        // file -> catalog
+                        var catalog = catalogItemGroup.Key;
+                        var physicalId = catalog.Id.TrimStart('/').Replace('/', '_');
+
+                        long groupId = -1;
+
+                        try
+                        {
+                            groupId = IOHelper.OpenOrCreateGroup(_fileId, physicalId).GroupId;
+
+                            // file -> catalog -> properties
+                            if (catalog.Properties.HasValue)
+                            {
+                                var key = "properties";
+                                var value = JsonSerializer.Serialize(catalog.Properties.Value, _serializerOptions);
+
+                                PrepareStringAttribute(groupId, key, value);
+                            }
+
+                            // file -> catalog -> resources
+                            foreach (var catalogItem in catalogItemGroup)
+                            {
+                                (var chunkLength, var chunkCount) = GeneralHelper.CalculateChunkParameters(totalLength);
+                                PrepareResource(groupId, catalogItem, chunkLength, chunkCount);
+                            }
+                        }
+                        finally
+                        {
+                            if (H5I.is_valid(groupId) > 0) { _ = H5G.close(groupId); }
+                        }
+                    }
+                }
+                finally
+                {
+                    _ = H5F.flush(_fileId, H5F.scope_t.GLOBAL);
+                }
+            }, cancellationToken);
+        }
+
+        public Task WriteAsync(
+            TimeSpan fileOffset,
+            WriteRequest[] requests,
+            IProgress<double> progress,
+            CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    var offset = (ulong)(fileOffset.Ticks / _lastSamplePeriod.Ticks);
+
+                    var requestGroups = requests
+                        .GroupBy(request => request.CatalogItem.Catalog)
+                        .ToList();
+
+                    var processed = 0;
+
+                    foreach (var requestGroup in requestGroups)
+                    {
+                        var catalog = requestGroup.Key;
+                        var physicalId = catalog.Id.TrimStart('/').Replace('/', '_');
+                        var writeRequests = requestGroup.ToArray();
+
+                        for (int i = 0; i < writeRequests.Length; i++)
+                        {
+                            cancellationToken.ThrowIfCancellationRequested();
+                            WriteData(physicalId, offset, writeRequests[i]);
+                        }
+
+                        processed++;
+                        progress.Report((double)processed / requests.Length);
+                    }
+                }
+                finally
+                {
+                    _ = H5F.flush(_fileId, H5F.scope_t.GLOBAL);
+                }
+            }, cancellationToken);
+        }
+
+        public Task CloseAsync(CancellationToken cancellationToken)
+        {
+            if (H5I.is_valid(_fileId) > 0) { _ = H5F.close(_fileId); }
+
+            return Task.CompletedTask;
+        }
+
+        private unsafe void WriteData(string catalogPhysicalId, ulong fileOffset, WriteRequest writeRequest)
+        {
+            long groupId = -1;
+            long datasetId = -1;
+            long dataspaceId = -1;
+            long dataspaceId_Buffer = -1;
+
+            try
+            {
+                var length = (ulong)writeRequest.Data.Length;
+                groupId = H5G.open(_fileId, $"/{catalogPhysicalId}/{writeRequest.CatalogItem.Resource.Id}");
+
+                var datasetName = $"dataset_{writeRequest.CatalogItem.Representation.Id}";
+                datasetId = H5D.open(groupId, datasetName);
+                dataspaceId = H5D.get_space(datasetId);
+                dataspaceId_Buffer = H5S.create_simple(1, new ulong[] { length }, null);
+
+                // dataset
+                _ = H5S.select_hyperslab(dataspaceId,
+                                    H5S.seloper_t.SET,
+                                    new ulong[] { fileOffset },
+                                    new ulong[] { 1 },
+                                    new ulong[] { 1 },
+                                    new ulong[] { length });
+
+                fixed (byte* bufferPtr = MemoryMarshal.AsBytes(writeRequest.Data.Span))
+                {
+                    if (H5D.write(datasetId, H5T.NATIVE_DOUBLE, dataspaceId_Buffer, dataspaceId, H5P.DEFAULT, new IntPtr(bufferPtr)) < 0)
+                        throw new Exception(ErrorMessage.Hdf5Writer_CouldNotWriteChunk_Dataset);
+                }
+            }
+            finally
+            {
+                if (H5I.is_valid(groupId) > 0) { _ = H5G.close(groupId); }
+                if (H5I.is_valid(datasetId) > 0) { _ = H5D.close(datasetId); }
+                if (H5I.is_valid(dataspaceId) > 0) { _ = H5S.close(dataspaceId); }
+                if (H5I.is_valid(dataspaceId_Buffer) > 0) { _ = H5S.close(dataspaceId_Buffer); }
+            }
+        }
+
+        private static void PrepareResource(long locationId, CatalogItem catalogItem, ulong chunkLength, ulong chunkCount)
+        {
+            long groupId = -1;
+            long datasetId = -1;
+
+            try
+            {
+                if (chunkLength <= 0)
+                    throw new Exception(ErrorMessage.Hdf5Writer_SampleRateTooLow);
+
+                groupId = IOHelper.OpenOrCreateGroup(locationId, catalogItem.Resource.Id).GroupId;
+
+                // file -> catalog -> resource -> properties
+                if (catalogItem.Resource.Properties.HasValue)
+                {
+                    var key = "properties";
+                    var value = JsonSerializer.Serialize(catalogItem.Resource.Properties.Value, _serializerOptions);
+
+                    PrepareStringAttribute(groupId, key, value);
+                }
+
+                // file -> catalog -> resource -> representation
+                datasetId = OpenOrCreateRepresentation(groupId, $"dataset_{catalogItem.Representation.Id}", chunkLength, chunkCount).DatasetId;
+            }
+            finally
+            {
+                if (H5I.is_valid(groupId) > 0) { _ = H5G.close(groupId); }
+                if (H5I.is_valid(datasetId) > 0) { _ = H5D.close(datasetId); }
+            }
+        }
+
+        // low level
+        private static (long DatasetId, bool IsNew) OpenOrCreateRepresentation(long locationId, string name, ulong chunkLength, ulong chunkCount)
+        {
+            long datasetId = -1;
+            GCHandle gcHandle_fillValue = default;
+            bool isNew;
+
+            try
+            {
+                var fillValue = Double.NaN;
+                gcHandle_fillValue = GCHandle.Alloc(fillValue, GCHandleType.Pinned);
+
+                (datasetId, isNew) = IOHelper.OpenOrCreateDataset(locationId, name, H5T.NATIVE_DOUBLE, chunkLength, chunkCount, gcHandle_fillValue.AddrOfPinnedObject());
+            }
+            catch (Exception)
+            {
+                if (H5I.is_valid(datasetId) > 0) { _ = H5D.close(datasetId); }
+
+                throw;
+            }
+            finally
+            {
+                if (gcHandle_fillValue.IsAllocated)
+                    gcHandle_fillValue.Free();
+            }
+
+            return (datasetId, isNew);
+        }
+
+        private static void PrepareStringAttribute(long locationId, string name, string value)
+        {
+            long typeId = -1;
+            long attributeId = -1;
+
+            bool isNew;
+
+            try
+            {
+                var classNamePtr = Marshal.StringToHGlobalAnsi(value);
+
+                typeId = H5T.copy(H5T.C_S1);
+                _ = H5T.set_size(typeId, new IntPtr(value.Length));
+
+                (attributeId, isNew) = IOHelper.OpenOrCreateAttribute(locationId, name, typeId, () =>
+                {
+                    long dataspaceId = -1;
+                    long localAttributeId = -1;
+
+                    try
+                    {
+                        dataspaceId = H5S.create(H5S.class_t.SCALAR);
+                        localAttributeId = H5A.create(locationId, name, typeId, dataspaceId);
+                    }
+                    finally
+                    {
+                        if (H5I.is_valid(dataspaceId) > 0) { _ = H5S.close(dataspaceId); }
+                    }
+
+                    return localAttributeId;
+                });
+
+                if (isNew)
+                    _ = H5A.write(attributeId, typeId, classNamePtr);
+            }
+            finally
+            {
+                if (H5I.is_valid(typeId) > 0) { _ = H5T.close(typeId); }
+                if (H5I.is_valid(attributeId) > 0) { _ = H5A.close(attributeId); }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ tests/Nexus.Writers.Hdf5.Tests/Hdf5Tests.cs
@@ -0,0 +1,138 @@
+using HDF5.NET;
+using Microsoft.Extensions.Logging.Abstractions;
+using Nexus.DataModel;
+using Nexus.Extensibility;
+using Xunit;
+
+namespace Nexus.Writers.Tests
+{
+    public class Hdf5Tests : IClassFixture<DataWriterFixture>
+    {
+        private readonly DataWriterFixture _fixture;
+
+        public Hdf5Tests(DataWriterFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task CanWriteFiles()
+        {
+            var targetFolder = _fixture.GetTargetFolder();
+            var dataWriter = new Hdf5() as IDataWriter;
+
+            var context = new DataWriterContext(
+                ResourceLocator: new Uri(targetFolder),
+                SystemConfiguration: default!,
+                RequestConfiguration: default!);
+
+            await dataWriter.SetContextAsync(context, NullLogger.Instance, CancellationToken.None);
+
+            var begin = new DateTime(2020, 01, 01, 0, 0, 0, DateTimeKind.Utc);
+            var samplePeriod = TimeSpan.FromSeconds(1);
+
+            var catalogItems = _fixture.Catalogs.SelectMany(catalog => catalog.Resources!
+                .SelectMany(resource => resource.Representations!.Select(representation => new CatalogItem(catalog, resource, representation))))
+                .ToArray();
+
+            var random = new Random(Seed: 1);
+
+            var length = 1000;
+
+            var data = new[]
+            {
+                Enumerable
+                    .Range(0, length)
+                    .Select(value => random.NextDouble() * 1e4)
+                    .ToArray(),
+
+                Enumerable
+                    .Range(0, length)
+                    .Select(value => random.NextDouble() * -1)
+                    .ToArray(),
+
+                Enumerable
+                    .Range(0, length)
+                    .Select(value => random.NextDouble() * Math.PI)
+                    .ToArray()
+            };
+
+            var requests = catalogItems
+                .Select((catalogItem, i) => new WriteRequest(catalogItem, data[i]))
+                .ToArray();
+
+            await dataWriter.OpenAsync(begin, TimeSpan.FromSeconds(2000), samplePeriod, catalogItems, CancellationToken.None);
+            await dataWriter.WriteAsync(TimeSpan.Zero, requests, new Progress<double>(), CancellationToken.None);
+            await dataWriter.WriteAsync(TimeSpan.FromSeconds(length), requests, new Progress<double>(), CancellationToken.None);
+            await dataWriter.CloseAsync(CancellationToken.None);
+
+            var actualFilePaths = Directory
+                .GetFiles(targetFolder)
+                .OrderBy(value => value)
+                .ToArray();
+
+            // assert
+            Assert.True(actualFilePaths.Length == 1);
+
+            using var h5File = H5File.OpenRead(actualFilePaths.First());
+
+            Assert.Equal(2, h5File.Children.Count());
+
+            // catalog 1
+            var catalog1 = h5File.Group("A_B_C");
+            var resources1 = _fixture.Catalogs[0].Resources!;
+            Assert.Equal(resources1.Count, catalog1.Children.Count());
+            var representations1 = _fixture.Catalogs[0].Resources!.SelectMany(resource => resource.Representations!).ToList();
+            Assert.Equal(representations1.Count, catalog1.Group("resource1").Children.Count());
+            // implement when HDF5.NET is able to read any struct easily
+            //AssertProperties(_fixture.Catalogs[0].Properties, catalog1.Properties);
+            //AssertProperties(_fixture.Catalogs[0].Resources[0].Properties, catalog1.Resources[0].Properties);
+            //AssertProperties(_fixture.Catalogs[0].Resources[0].Properties, catalog1.Resources[1].Properties);
+
+            var actualData0 = catalog1
+                .Group("resource1")
+                .Dataset("dataset_1_s")
+                .Read<double>();
+
+            Assert.True(data[0].Concat(data[0]).SequenceEqual(actualData0));
+
+            var actualData1 = catalog1
+                .Group("resource1")
+                .Dataset("dataset_2_s")
+                .Read<double>();
+
+            Assert.True(data[1].Concat(data[1]).SequenceEqual(actualData1));
+
+            // catalog 2
+            var catalog2 = h5File.Group("D_E_F");
+            var resources2 = _fixture.Catalogs[0].Resources;
+            Assert.Equal(resources2!.Count, catalog2.Children.Count());
+            var representations2 = _fixture.Catalogs[1].Resources!.SelectMany(resource => resource.Representations!).ToList();
+            Assert.Equal(representations2.Count, catalog2.Group("resource3").Children.Count());
+            // implement when HDF5.NET is able to read any struct easily
+            //AssertProperties(_fixture.Catalogs[1].Properties, catalog2.Properties);
+            //AssertProperties(_fixture.Catalogs[1].Resources[0].Properties, catalog2.Resources[0].Properties);
+
+            var actualData2 = catalog2
+                .Group("resource3")
+                .Dataset("dataset_1_s")
+                .Read<double>();
+
+            Assert.True(data[2].Concat(data[2]).SequenceEqual(actualData2));
+
+            //void AssertProperties(IReadOnlyDictionary<string, string> expected, List<xxx> actual)
+            //{
+            //    Assert.Equal(expected.Count, actual.Count);
+
+            //    for (int i = 0; i < actual.Count; i++)
+            //    {
+            //        var key = actual[i].Name;
+            //        var value = actual[i].Value;
+
+            //        var match = expected[key];
+            //        Assert.Equal(match, value);
+            //    }
+            //}
+        }
+    }
+}
